2c4b30854ca92d3cb48a8c32b7db1c860e858bc5
mizuki.yasuda <mizuki.yasuda@bigtreetc.com>
2021-02-25 05:44:41 +0900

    React_Docs 2.フック早わかり

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
         <a
           className="App-link"
           href="https://reactjs.org"
